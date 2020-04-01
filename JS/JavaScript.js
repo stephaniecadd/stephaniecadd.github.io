ab698dc774f7b36b548f6e1f3ab498b2e086e4ce
stephaniecadd <stephaniecadd94@gmail.com>
2020-04-01 14:24:12 -0700

    Update Portfolio

---
+++ JS/JavaScript.js
@@ -0,0 +1,35 @@
+//adds a smooth scrolling
+
+$(document).ready(function(){   
+    $("a").on('click', function(event) {
+        
+      // Make sure this.Link has a value before overriding default behavior
+      if (this.Link !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store Link
+        var Link = this.Link;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        //number of milliseconds
+        $('html, body').animate({
+          scrollTop: $(Link).offset().top
+        }, 2000, function(){
+     
+          // Add Link (#) to URL when done scrolling (default click behavior)
+          window.location.Link = Link;
+        });
+      } // End if
+    });
+  });
+  
+  // These functions open and close the contact form 
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    this.CloseForm();
+  }
