c0a35dce327796bb3557a57699fbe25d901c0fd3
stephaniecadd <stephaniecadd94@gmail.com>
2020-06-29 11:50:51 -0700

    Hover Effect

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -7,30 +7,7 @@
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
-//adds a smooth scrolling
 
-$(document).ready(function(){   
-    $("a").on('click', function(event) {
-        
-      if (this.Link !== "#Slideshow") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store Link
-        var Link = this.Link;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        //number of milliseconds
-        $('html, body').animate({
-          scrollTop: $(Link).offset().top
-        }, 2000, function(){
-     
-          // Add Link (#) to URL when done scrolling (default click behavior)
-          window.location.Link = Link;
-        });
-      } // End if
-    });
-  });
   //comparison slider//
   function initComparisons() {
     var x, i;
